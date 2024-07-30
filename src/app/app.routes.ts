e7a3cdce52884b5e7fa7b3ab39c55d8f8e9bd62e
Yoana Marrero <yoanamarreropro@gmail.com>
2024-07-30 23:48:57 +0100

    Creación de los componentes, servicios y estructuras necesarias para iniciar el proyecto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+// Importamos todos los componentes  de las vistas
+import { LoginComponent } from './views/login/login.component';
+import { DashboardComponent } from './views/dashboard/dashboard.component';
+import { UserNewComponent } from './views/user-new/user-new.component';
+import { UserEditComponent } from './views/user-edit/user-edit.component';
+
+export const routes: Routes = [
+    {path:'', redirectTo:'login', pathMatch:'full'},
+    {path:'login', component:LoginComponent},
+    {path:'dashboard', component:DashboardComponent},
+    {path:'user-new', component:UserNewComponent},
+    {path:'user-edit', component:UserEditComponent}
+];
+  
---
+++ src/app/views/user-new/user-new.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-new',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-new.component.html',
+  styleUrl: './user-new.component.css'
+})
+export class UserNewComponent {
+
+}
